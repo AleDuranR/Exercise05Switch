5d757a1eb7155706dd2c048fc847872d0f68be37
AleDuranR <aduranrocha@gmail.com>
2019-10-14 12:13:13 -0500

    Finish project.

--- src/com/exercise05switch/app/SwitchStatement.java
+++ src/com/exercise05switch/app/SwitchStatement.java
@@ -8,62 +8,69 @@ public class SwitchStatement {
 		// Constant declaration
 		// Variables declaration
 		int opc = 0;
-		
+		char answer = 'y';
 		// Object construction
 		Scanner scan = new Scanner(System.in);
 		
 		// Input
-		// Input data validation
-		while(opc <= 0) {
-			System.out.println("Input a number of the month ");
-			opc = scan.nextInt();
-			// When the number is under 0
-			if(opc <= 0) {
-				System.out.println("ERROR: That month doesn't exist");
-			}
-		}	
+		do {
+			opc = 0;
+			// Input data validation
+			while(opc <= 0) {
+				System.out.println("Input a number of the month ");
+				opc = scan.nextInt();
+				// When the number is under 0
+				if(opc <= 0) {
+					System.out.println("ERROR: That month doesn't exist");
+				}
+			}	
 			// Process
 			switch(opc) {
-				case 1:
-					System.out.print("January");
-					break;
-				case 2:
-					System.out.print("Feburary");
-					break;
-				case 3:
-					System.out.print("March");
-					break;
-				case 4:
-					System.out.print("April");
-					break;
-				case 5:
-					System.out.print("May");
-					break;
-				case 6:
-					System.out.print("June");
-					break;
-				case 7:
-					System.out.print("July");
-					break;
-				case 8:
-					System.out.print("August");
-					break;
-				case 9:
-					System.out.print("September");
-					break;
-				case 10:
-					System.out.print("October");
-					break;
-				case 11:
-					System.out.print("November");
-					break;
-				case 12:
-					System.out.print("December");
-					break;				
-				default: 
-					System.out.println("ERROR: Invalid number");
-					break;
+			case 1:
+				System.out.println("January");
+				break;
+			case 2:
+				System.out.println("Feburary");
+				break;
+			case 3:
+				System.out.println("March");
+				break;
+			case 4:
+				System.out.println("April");
+				break;
+			case 5:
+				System.out.println("May");
+				break;
+			case 6:
+				System.out.println("June");
+				break;
+			case 7:
+				System.out.println("July");
+				break;
+			case 8:
+				System.out.println("August");
+				break;
+			case 9:
+				System.out.println("September");
+				break;
+			case 10:
+				System.out.println("October");
+				break;
+			case 11:
+				System.out.println("November");
+				break;
+			case 12:
+				System.out.println("December");
+				break;				
+			default: 
+				System.out.println("ERROR: Invalid number");
+				break;
 			}
+			System.out.println("Will you like to keep asking? (y/n) ");
+			// capture the value in the position 0
+			answer = scan.next().charAt(0);
+			
+		}while(answer == 'y');
 		scan.close();
 	}
 }
